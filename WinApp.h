95119a01000109d9044a63fad5e76e66f8f8b6eb
2002五月女 <94036178+2002saotome@users.noreply.github.com>
2022-12-09 15:09:28 +0900

    03_03_windowsAPIのクラス化_初期化編2

--- WinApp.h
+++ WinApp.h
@@ -11,4 +11,19 @@ public:  //メンバー関数
 	void Initialize();
 	//更新
 	void Update();
+
+	//getter
+	HWND GetHwnd()const { return hwnd;}
+public: //定数
+	// ウィンドウサイズ
+	 static const int window_width = 1280;  // 横幅
+	 static const int window_height = 720;  // 縦幅
+private:
+	//ウィンドウハンドル
+	HWND hwnd = nullptr;
+	//getter
+	HINSTANCE GetHInstance()const { return w.hInstance; }
+private:
+	//ウィンドウクラスの設定
+	WNDCLASSEX w{};
 };
