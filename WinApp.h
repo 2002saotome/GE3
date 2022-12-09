a3c5d3cab9fbef93f26b754ae9379c1dfa68fa67
2002五月女 <94036178+2002saotome@users.noreply.github.com>
2022-12-09 14:23:18 +0900

    03_02_WindowsAPIのクラス化_初期化編1

--- WinApp.h
+++ WinApp.h
@@ -1,7 +1,11 @@
+#include<Windows.h>
 #pragma once
 
 class WinApp
 {
+public: //静的メンバ関数
+	static LRESULT WindowProc(HWND hwnd, UINT msg, WAPRAM wparam, LPARAM lparam);
+
 public:  //メンバー関数
 	//初期化
 	void Initialize();
